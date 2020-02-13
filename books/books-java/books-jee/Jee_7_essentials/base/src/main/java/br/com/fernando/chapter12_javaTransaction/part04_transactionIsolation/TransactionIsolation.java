1bb7b92ccac0225e86688b5ca5eb168f2cbbcff6
Fernando <fernando.romulo.silva@gmail.com>
2020-02-13 06:45:30 -0300

    news

--- books/books-java/books-jee/Jee_7_essentials/base/src/main/java/br/com/fernando/chapter12_javaTransaction/part04_transactionIsolation/TransactionIsolation.java
+++ books/books-java/books-jee/Jee_7_essentials/base/src/main/java/br/com/fernando/chapter12_javaTransaction/part04_transactionIsolation/TransactionIsolation.java
@@ -1,5 +1,6 @@
 package br.com.fernando.chapter12_javaTransaction.part04_transactionIsolation;
 
+//TODO
 public class TransactionIsolation {
 
     // https://emmanuelneri.com.br/2016/04/03/abrindo-novas-transacoes-dentro-de-metodos-transacionais/
--- books/books-java/books-jee/Jee_7_essentials/base/src/main/java/br/com/fernando/chapter17_javaMail/part00_intro/JavaMail.java
+++ books/books-java/books-jee/Jee_7_essentials/base/src/main/java/br/com/fernando/chapter17_javaMail/part00_intro/JavaMail.java
@@ -1,5 +1,6 @@
 package br.com.fernando.chapter17_javaMail.part00_intro;
 
+// TODO
 public class JavaMail {
 
 }
