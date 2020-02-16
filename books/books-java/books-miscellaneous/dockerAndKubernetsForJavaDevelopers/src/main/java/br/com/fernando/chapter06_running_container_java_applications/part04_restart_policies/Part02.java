b6c14f379343d0bf5f51c2a6b8ef129a3eac1c1d
Fernando <fernando.romulo.silva@gmail.com>
2020-02-16 13:00:06 -0300

    news

--- books/books-java/books-miscellaneous/dockerAndKubernetsForJavaDevelopers/src/main/java/br/com/fernando/chapter06_running_container_java_applications/part04_restart_policies/Part02.java
+++ books/books-java/books-miscellaneous/dockerAndKubernetsForJavaDevelopers/src/main/java/br/com/fernando/chapter06_running_container_java_applications/part04_restart_policies/Part02.java
@@ -2,12 +2,26 @@ package br.com.fernando.chapter06_running_container_java_applications.part04_res
 
 public class Part02 {
 
-	// unless-stopped
+	// Updating a restart policy on a running container
 	//
-	// The unless-stopped restart policy acts the same as always with one exception, it will restart the container regardless 
-	// of the exit status, but do not start it on daemon startup if the container has been put to a stopped state before
+	// Sometimes, there's a need to update the Docker runtime parameters after the container has already started, on the fly.
+	//
+	// Apart from other runtime parameters (such as memory or CPU constraints for example, which we are going to discuss later in this chapter), 
+	// the docker update command gives you the option to update the restart policy on a running container. 
+	//
+	// The syntax is quite straightforward, you just need to provide the new restart policy that you would like the container to have and the container's ID or name:
+	//
+	// $ docker update --restart=always <CONTAINER_ID or NAME>
+	//
+	//  if you execute the update command on a container that is stopped, the policy will be used when you start the container later on. 
+	// The possible options are exactly the same as those you can specify when starting the container:
+	//
+	// * no (which is default)
+	// * always
+	// * on-failure
+	// * unless-stopped
 	// 
-	// Before you apply the restart policy to your container, it's good to think first what kind of work the container will be used to do. 
+
 
 	
 }
