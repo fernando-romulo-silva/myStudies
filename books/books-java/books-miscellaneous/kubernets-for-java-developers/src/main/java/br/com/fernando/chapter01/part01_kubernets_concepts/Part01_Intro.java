d1b1c8425637ae19ccc02fe655a6ac2955e2720b
Fernando <fernando.romulo.silva@gmail.com>
2020-03-06 07:48:40 -0300

    news

---
+++ books/books-java/books-miscellaneous/kubernets-for-java-developers/src/main/java/br/com/fernando/chapter01/part01_kubernets_concepts/Part01_Intro.java
@@ -0,0 +1,8 @@
+package br.com.fernando.chapter01.part01_kubernets_concepts;
+
+public class Part01_Intro {
+
+    // Kubernetes is an open source orchestration system for managing containerized applications across multiple hosts, providing basic mechanisms 
+    // for the deployment, maintenance, and scaling of applications.
+    
+}
---
+++ books/books-java/books-miscellaneous/kubernets-for-java-developers/src/main/java/br/com/fernando/chapter01/part01_kubernets_concepts/Part02_Pods.java
@@ -0,0 +1,13 @@
+package br.com.fernando.chapter01.part01_kubernets_concepts;
+
+public class Part02_Pods {
+    
+    // A pod is the smallest deployable unit that can be created, scheduled, and managed. It’s a logical collection of containers that belong to an application. 
+    // Pods are created in a namespace. 
+    // All containers in a pod share the namespace, volumes, and networking stack. 
+    // This allows containers in the pod to “find” each other and communicate using localhost.
+    //
+    // Each resource in Kubernetes can be defined using a configuration file. 
+    // For example, a WildFly pod can be defined with the configuration file shown in 
+
+}
