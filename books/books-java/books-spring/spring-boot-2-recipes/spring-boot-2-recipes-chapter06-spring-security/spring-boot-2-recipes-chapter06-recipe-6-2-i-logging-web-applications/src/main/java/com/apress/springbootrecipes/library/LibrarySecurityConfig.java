d3b914eaf3f11ce91f7f448bb3e8965cbbd4013a
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-06-03 17:07:03 -0300

    news

---
+++ books/books-java/books-spring/spring-boot-2-recipes/spring-boot-2-recipes-chapter06-spring-security/spring-boot-2-recipes-chapter06-recipe-6-2-i-logging-web-applications/src/main/java/com/apress/springbootrecipes/library/LibrarySecurityConfig.java
@@ -0,0 +1,58 @@
+package com.apress.springbootrecipes.library;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LibrarySecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    public LibrarySecurityConfig() {
+	super(true); // disable default configuration
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+	registry.addViewController("/login.html").setViewName("login");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+	http.securityContext() //
+			.and().exceptionHandling() //
+			.and().servletApi() //
+			.and().httpBasic() //
+			.and().logout().logoutSuccessUrl("/").and().headers() //
+			.and().csrf() //
+			//
+			.and().anonymous().principal("guest").authorities("ROLE_GUEST") //
+			.and().rememberMe() //
+			//
+			.and().formLogin() //
+			.loginPage("/login.html").defaultSuccessUrl("/") //
+			//
+			.failureUrl("/login.html?error=true").permitAll() //
+			.and().authorizeRequests() //
+			.mvcMatchers("/").permitAll() //
+			.anyRequest().authenticated(); //
+    }
+
+    @Bean
+    public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+	StrictHttpFirewall firewall = new StrictHttpFirewall();
+	firewall.setAllowUrlEncodedSlash(true);
+	return firewall;
+    }
+
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        super.configure(web);
+        web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
+    }
+}
