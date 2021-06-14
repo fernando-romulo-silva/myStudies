90b48e1314ff26808ad44911ca4d8d7dfef1060c
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-06-14 11:20:45 -0300

    news

--- books/books-java/books-spring/spring-boot-2-recipes/temp/ch07/recipe_7_1_iv/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO customer (name, email) VALUES
-                                          ('Marten Deinum', 'marten.deinum@conspect.nl'),
-                                          ('Josh Long', 'jlong@pivotal.com'),
-                                          ('John Doe', 'john.doe@island.io'),
-                                          ('Jane Doe', 'jane.doe@island.io');
