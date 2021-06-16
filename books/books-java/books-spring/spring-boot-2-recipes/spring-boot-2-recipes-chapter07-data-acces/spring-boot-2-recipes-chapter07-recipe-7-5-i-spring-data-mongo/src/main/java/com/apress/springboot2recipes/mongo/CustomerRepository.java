e398a947734f327bbaa78e92df015dbb0692abd5
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-06-16 13:59:43 -0300

    news

--- books/books-java/books-spring/spring-boot-2-recipes/spring-boot-2-recipes-chapter07-data-acces/spring-boot-2-recipes-chapter07-recipe-7-5-i-spring-data-mongo/src/main/java/com/apress/springboot2recipes/mongo/CustomerRepository.java
+++ books/books-java/books-spring/spring-boot-2-recipes/spring-boot-2-recipes-chapter07-data-acces/spring-boot-2-recipes-chapter07-recipe-7-5-i-spring-data-mongo/src/main/java/com/apress/springboot2recipes/mongo/CustomerRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public interface CustomerRepository {
 
-	List<Customer> findAll();
-	Customer findById(long id);
-	Customer save(Customer customer);
+    List<Customer> findAll();
+
+    Customer findById(long id);
+
+    Customer save(Customer customer);
 }
