349bf4f45d09566bf24715ed61213e8e757ab44e
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-06-28 16:42:31 -0300

    news

--- books/books-others/docker-certified-associate-exam-guide/chapter05-deploying-multi-container-applications/simplest-lab/simplestlb/entrypoint.sh
+++ books/books-others/docker-certified-associate-exam-guide/chapter05-deploying-multi-container-applications/simplest-lab/simplestlb/entrypoint.sh
@@ -5,6 +5,6 @@ CONFIGFILE="/etc/nginx/nginx.conf"
 sed -i "s/__APPLICATION_ALIAS__/${APPLICATION_ALIAS}/g" ${CONFIGFILE}
 sed -i "s/__APPLICATION_PORT__/${APPLICATION_PORT}/g" ${CONFIGFILE}
 
-#EXECUTE CMD ;P
+EXECUTE CMD ;P
 
 exec "$@"
