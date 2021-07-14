d8f47aa93e9dc85f71fdbc1e80adcd1951026216
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-07-14 14:42:51 -0300

    news

--- books/books-java/books-quarkus/understanding-quarkus/understanding-quarkus-chapter06-http/understanding-quarkus-chapter06-part06-open-api-advanced/src/main/java/org/agoncal/fascicle/quarkus/http/openapi/advanced/BookService.java
+++ books/books-java/books-quarkus/understanding-quarkus/understanding-quarkus-chapter06-http/understanding-quarkus-chapter06-part06-open-api-advanced/src/main/java/org/agoncal/fascicle/quarkus/http/openapi/advanced/BookService.java
@@ -7,26 +7,26 @@ import java.util.Optional;
 @ApplicationScoped
 public class BookService {
 
-  public Book persistBook(Book book) {
-    return null;
-  }
+    public Book persistBook(Book book) {
+	return null;
+    }
 
-  public List<Book> findAllBooks() {
-    return null;
-  }
+    public List<Book> findAllBooks() {
+	return null;
+    }
 
-  public Optional<Book> findBookById(Long id) {
-    return null;
-  }
+    public Optional<Book> findBookById(Long id) {
+	return null;
+    }
 
-  public Book findRandomBook() {
-    return null;
-  }
+    public Book findRandomBook() {
+	return null;
+    }
 
-  public Book updateBook(Book book) {
-    return null;
-  }
+    public Book updateBook(Book book) {
+	return null;
+    }
 
-  public void deleteBook(Long id) {
-  }
+    public void deleteBook(Long id) {
+    }
 }
