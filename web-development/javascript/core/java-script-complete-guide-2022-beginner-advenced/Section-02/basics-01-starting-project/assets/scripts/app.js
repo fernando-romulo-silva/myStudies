684c5e8f61540199cef76e2ccf940e407e3585f5
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2022-08-08 08:57:01 +0100

    news

--- web-development/javascript/core/java-script-complete-guide-2022-beginner-advenced/Section-02/basics-01-starting-project/assets/scripts/app.js
+++ web-development/javascript/core/java-script-complete-guide-2022-beginner-advenced/Section-02/basics-01-starting-project/assets/scripts/app.js
@@ -1,10 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+currentResult = add(1, 2);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-let errorMessage = 'An error occured!';
-
-outputResult(currentResult, errorMessage);
+outputResult(currentResult, calculationDescription);
